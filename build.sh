fb1f01cf16202be4e5f8256766a62750da63cf93
Yuji Yokoo <yujiyokoo@gmail.com>
2025-07-07 08:55:43 +0900

    fix: add platform to docker command

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 mrbc -B bytecode --verbose add.rb
 
-docker run --rm -i -t -v $(pwd):/src z88dk/z88dk zcc +sms -vn test_prog.c mrubyz.c add.c -o test_prog -create-app
+docker run --platform linux/amd64 --rm -i -t -v $(pwd):/src z88dk/z88dk zcc +sms -vn test_prog.c mrubyz.c add.c -o test_prog -create-app
