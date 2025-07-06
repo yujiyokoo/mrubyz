6a592d1510de2e98582a693e217d9691e46f8f8f
Yuji Yokoo <yujiyokoo@gmail.com>
2025-07-06 17:35:12 +0900

    feat: Add super-simple integer test

---
+++ tests/main.c
@@ -0,0 +1,10 @@
+#include "test.h"
+
+int main(int argc, char *argv[])
+{
+    int  res = 0;
+
+    res += test_integer();
+
+    return res;
+}
---
+++ tests/test_integer.c
@@ -0,0 +1,22 @@
+#include "test.h"
+#include "../mrubyz.h"
+#include <stdio.h>
+
+extern uint8_t bytecode[];
+void test_integer_load_return() {
+  mrbz_val v;
+  mrbz_vm vm;
+  mrbz_vm_run(&vm, &v, bytecode);
+
+  printf("returned num: %d\n", v.value);
+  Assert(v.value == 1, "integer assertion failed");
+}
+
+int test_integer()
+{
+  suite_setup("Trivial integer test");
+  suite_add_test(test_integer_load_return);
+
+  return suite_run();
+}
+
