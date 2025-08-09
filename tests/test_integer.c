5e4a9c5401a33d01905a82f102d3d37b65716a89
Yuji Yokoo <yujiyokoo@gmail.com>
2025-08-09 16:23:55 +0900

    test: test loadi instruction

--- tests/test_integer.c
+++ tests/test_integer.c
@@ -3,6 +3,15 @@
 #include <stdio.h>
 
 extern uint8_t add[];
+extern uint8_t loadi[];
+void test_integer_load_return() {
+  mrbz_val v;
+  mrbz_vm vm;
+  mrbz_vm_run(&vm, &v, loadi);
+
+  printf("returned num: %d\n", v.value);
+  Assert(v.value == 5, "integer load failed");
+}
 
 void test_integer_add() {
   mrbz_val v;
@@ -10,12 +19,13 @@ void test_integer_add() {
   mrbz_vm_run(&vm, &v, add);
 
   printf("returned num: %d\n", v.value);
-  Assert(v.value == 6, "integer assertion failed");
+  Assert(v.value == 6, "integer add failed");
 }
 
 int test_integer()
 {
   suite_setup("integer tests");
+  suite_add_test(test_integer_load_return);
   suite_add_test(test_integer_add);
 
   return suite_run();
