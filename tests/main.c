e1826518d3742f25e2c66962b9158f347dfc0727
Yuji Yokoo <yujiyokoo@gmail.com>
2025-09-10 00:57:37 +0900

    feat: introduce mrbz_val while still only supportig integers

--- tests/main.c
+++ tests/main.c
@@ -1,10 +1,12 @@
 #include "test_integer.h"
+#include "test_string.h"
 
 int main(int argc, char *argv[])
 {
-    int  res = 0;
+  int  res = 0;
 
-    res += test_integer();
+  res += test_integer();
+  res += test_string();
 
-    return res;
+  return res;
 }
