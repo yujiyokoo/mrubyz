a37488d5ef94860993e8d218cee82aef796b0a3e
Yuji Yokoo <yujiyokoo@gmail.com>
2025-10-19 15:34:49 +1100

    feat: Add forgotten test_loop files

---
+++ tests/test_loop.c
@@ -0,0 +1,20 @@
+#include "test.h"
+#include "../mrubyz.h"
+
+extern uint8_t while_simple[];
+
+void test_while_simple() {
+  mrbz_val v;
+  mrbz_vm vm;
+  mrbz_vm_run(&vm, &v, while_simple);
+
+  Assert(T_INT == v.type, "while example did not rutern int");
+  Assert(1 == v.intval, "while example did not rutern 1");
+}
+
+int test_loop() {
+  suite_setup("loop tests");
+  suite_add_test(test_while_simple);
+
+  return suite_run();
+}
