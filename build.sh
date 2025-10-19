32045b950f78212aea0168c220318a190c0d9856
Yuji Yokoo <yujiyokoo@gmail.com>
2025-10-19 15:36:17 +1100

    feat: Add malloc to building demo program

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 mrbc -B bytecode --verbose -o ./add.ruby.c tests/ruby/add.rb
 
-docker run --platform linux/amd64 --rm -i -t -v $(pwd):/src z88dk/z88dk zcc +sms -vn test_prog.c mrubyz.c add.ruby.c -o test_prog -create-app
+docker run --platform linux/amd64 --rm -i -t -v $(pwd):/src z88dk/z88dk zcc +sms -vn test_prog.c mrubyz.c add.ruby.c -o test_prog -create-app -DAMALLOC
