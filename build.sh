597cbf40bbe8c7ee8f5cf882d71ec6a94ba21307
Yuji Yokoo <yujiyokoo@gmail.com>
2025-11-09 18:16:13 +0900

    chore: add `-m` option to build (memory map)

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 mrbc -B bytecode --verbose -o ./demo/add.ruby.c demo/add.rb
 mrbc -B demo02_bytecode --verbose -o ./demo/demo02.ruby.c demo/demo02.rb
 
-docker run --platform linux/amd64 --rm -i -t -v $(pwd):/src z88dk/z88dk zcc +sms -vn demo01.c mrubyz.c demo/add.ruby.c -o demo01 -create-app -DAMALLOC
-docker run --platform linux/amd64 --rm -i -t -v $(pwd):/src z88dk/z88dk zcc +sms -vn demo02.c mrubyz.c demo/demo02.ruby.c -o demo02 -create-app -DAMALLOC
+docker run --platform linux/amd64 --rm -i -t -v $(pwd):/src z88dk/z88dk zcc +sms -vn -m demo01.c mrubyz.c demo/add.ruby.c -o demo01 -create-app -DAMALLOC
+docker run --platform linux/amd64 --rm -i -t -v $(pwd):/src z88dk/z88dk zcc +sms -vn -m demo02.c mrubyz.c demo/demo02.ruby.c -o demo02 -create-app -DAMALLOC
