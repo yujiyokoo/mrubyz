09fe64ede5d9ef14dc8f9ae30090a69534031a7b
Yuji Yokoo <yujiyokoo@gmail.com>
2026-01-27 01:31:42 +0900

    feat: add navigating back

--- demo/demo04.rb
+++ demo/demo04.rb
@@ -6,7 +6,7 @@ pres_data = [
 
 i = 0
 prev_btns = 0xFF
-while pres_data.size > i
+while true
   j = 0
   clear_screen
   gotoxy(0, 0)
@@ -15,17 +15,30 @@ while pres_data.size > i
   puts pres_data[i][j] if pres_data[i].size > j
   j += 1
 
+  going_back = false
   # + 1 since we want to wait for keypress after last sentence
   while pres_data[i].size + 1 > j
     btns = read_buttons
+    # A button
     if btns & 0x10 == 0 && prev_btns & 0x10 != 0
       # Don't try to display if we've gone past the end
       # We just wanted to wait for the keypress
       puts pres_data[i][j] if pres_data[i].size > j
       j += 1
     end
+
+    # B button
+    if btns & 0x20 == 0 && prev_btns & 0x20 != 0
+      j = pres_data[i].size + 1 # instead of break
+      i -= 1 unless i == 0
+      going_back = true
+    end
     prev_btns = btns
   end
 
-  i += 1
+  # go forward, unless going back (B pressed)
+  i += 1 unless going_back
+
+  # don't go past the last page, so we can go back in navigation
+  i -= 1 if pres_data.size == i
 end
