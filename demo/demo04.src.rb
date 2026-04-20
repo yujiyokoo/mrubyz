62c088eed07d9ccf2f9a4fdefdbd7d3f24c67b9b
Yuji Yokoo <yujiyokoo@gmail.com>
2026-04-21 01:47:15 +0900

    fix: Make going back in page work correctly

--- demo/demo04.src.rb
+++ demo/demo04.src.rb
@@ -52,7 +52,6 @@ while true
   puts pres_data[i][j] if pres_data[i].size > j
   j += 1
 
-  going_back = false
   current_col = 0
   # + 1 since we want to wait for keypress after last sentence
   while pres_data[i].size + 1 > j
@@ -61,7 +60,14 @@ while true
     if btns & 0x10 == 0 && prev_btns & 0x10 != 0
       # Don't try to display if we've gone past the end
       # We just wanted to wait for the keypress
-      puts pres_data[i][j] if pres_data[i].size > j
+      if pres_data[i].size > j
+        if pres_data[i][j] == "VMRUN\r"
+          print "        "
+          print vmrun
+        else
+          puts pres_data[i][j]
+        end
+      end
       j += 1
     end
 
@@ -82,13 +88,23 @@ while true
     if btns & 0x20 == 0 && prev_btns & 0x20 != 0
       j = pres_data[i].size + 1 # instead of break
       i -= 1 unless i == 0
-      going_back = true
+      j = 0
+			clear_screen
+      if pres_data[i].size > j
+        if pres_data[i][j] == "VMRUN\r"
+          print "        "
+          print vmrun
+        else
+          puts pres_data[i][j]
+        end
+      end
+			j += 1
     end
     prev_btns = btns
   end
 
   # go forward, unless going back (B pressed)
-  i += 1 unless going_back
+  i += 1
 
   # don't go past the last page, so we can go back in navigation
   i -= 1 if pres_data.size == i
